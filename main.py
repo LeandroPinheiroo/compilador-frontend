90a901e1d2b45b2a8722872f8cde52b5b2cd34d4
leandro <leandropinheiro98@hotmail.com>
2019-10-30 09:47:37 -0300

    Prepara compilador para receber por parametro o arquivo de entrada a ser compilado conforme especificacoes do trabalho alem de inserir comentarios na classe Main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
+########################################################################
+# João Paulo de Souza    - 0035329                                     #
+# Leandro Souza Pinheiro - 0015137                                     #
+# Trabalho Compiladores  - Front-end do compilador para a linguagem P  #
+########################################################################
+
+#Importacoes
 import Scanner as scanner
 import Type as type
 import Parser as parser
 import sys as s
 
-
+#MAIN
 if __name__ == "__main__":
-    """
-    scaninho = scanner.Scanner('exemplo1.txt')
-    scaninho.open_file()
-    token = scaninho.getToken()
-    while(token.type != type.Type().FIMARQ):
-        print(str(token.type) , " -> " , str(token.line))
-        token = scaninho.getToken()
-    """
-    arg = s.argv
-    parser = parser.Parser()
-    parser.interpreter('./2019923_233811_compiladores1-trabalho1-2019-2/testes-trabalho-1-2019-2/exemplo1.txt')
-    if (len(arg) == 2):
-        if (arg[1] == '-p'):
-            parser.symbols_table.printTable()
-        else:
+    arg = s.argv # recebe os parametros passados por linha de comando
+    parser = parser.Parser() #chama o parser
+    if (len(arg) == 2): # caso a quantidade de argumentos seja 2 indica que nao precisa printar a tabela de simbolos
+        parser.interpreter(arg[1]) # chama metodo para realizar interpretacao do arquivo passado
+        #arquivo a ser interpretado se encontra na posicao 1 do vetor de argumentos
+    elif (len(arg) == 4): #caso a quantidade de argumentos seja 4 indica que precisa printar a tabela de simbolos no arquivo 
+        if (arg[2] == '-t'): # obrigatoriamente na posicao 2 dos argumentos necessariamente deve ser -t
+            parser.interpreter(arg[1])# chama metodo para realizar interpretacao do arquivo passado
+            #arquivo a ser interpretado se encontra na posicao 1 do vetor de argumentos
+            fileName = arg[2] #recebe o arquivo de saida que conterá a tabela de simbolos
+            parser.symbols_table.exportToFile(fileName) # chama metodo para exportar a tabela de simbolos para o arquivo desejado
+        else:#caso contrario foi passado um comando invalido
             print('Comando inválido')
-    elif (len(arg) == 3):
-        if (arg[1] == '-t'):
-            fileName = arg[2]
-            parser.symbols_table.exportToFile(fileName)
-        else:
-            print('Comando inválido')
+    else:#caso contrario foi passado uma quantidade e argumentos invalido
+        print("Número invalido de argumentos passados na linha de comando")
